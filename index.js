63881d9512687e3ff3831d60039bb78ffb9af509
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-01-16 23:53:30 -0500

    style and about

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var area = turf.area;
 
 var mainDiv = document.getElementById('gallery');
 
-var w = 400,
-    h = 400;
+var w = 300,
+    h = 300;
 
 var projection = d3.geo.mercator()
     .translate([w / 2, h / 2]);
@@ -15,8 +15,7 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path()
     .projection(projection);
 
-function render(ls, id)
-{
+function render(ls, id) {
     var div = document.createElement('div');
     mainDiv.appendChild(div);
     div.setAttribute('id', 'r' + id);
@@ -38,15 +37,13 @@ function render(ls, id)
         .attr('d', path)
         .attr('x', 100)
         .attr('y', 100)
-        .style('color', '#000')
         .style('stroke', '#000')
         .style('stroke-width', '0.5px')
         .style('fill', 'none');
 
 }
 
-function main(error, data)
-{
+function main(error, data) {
     if (error) {
         throw error;
     }
@@ -56,6 +53,19 @@ function main(error, data)
     for (var i = 0; i < feats.length; i++) {
         render(feats[i], i);
     }
+}
+
+var icon = document.getElementById('about-icon');
+var about = document.getElementById('about');
+icon.addEventListener('mouseover', iconMouseOver);
+icon.addEventListener('mouseleave', iconMouseLeave);
+
+function iconMouseOver() {
+    about.style.visibility = 'visible';
+}
+
+function iconMouseLeave() {
+    about.style.visibility = 'hidden';
 }
 
 d3.json('./runs.geojson', main);
